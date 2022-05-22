fcf80b0244d086c83b63eebb3b6ca97998277c54
Soumya Mukherjee <rivu10@gmail.com>
2022-05-22 13:08:11 +0530

    Change of Try-with-resource implementation

---
+++ Java9Features/src/main/java/com/example/java9/two/TryWithResource.java
@@ -0,0 +1,46 @@
+package com.example.java9.two;
+
+class Resource{
+
+    public Resource() { System.out.println("Creating external resources"); }
+
+    public void op1() { System.out.println("Op1 is called"); }
+
+    public void finalize() { System.out.println("Cleaning up external resources.."); }
+}
+
+class ResourceAfterJava9 implements AutoCloseable{
+
+    public ResourceAfterJava9() { System.out.println("Creating external resources"); }
+
+    public void op1() { System.out.println("Op1 is called"); }
+
+    public void close() { System.out.println("Cleaning up external resources.."); }
+}
+
+public class TryWithResource {
+    public static void main(String[] args) {
+
+        Resource resource = new Resource();
+        resource.op1(); // finalize method will not be called immediately as JVM has sufficient memory
+
+        System.out.println("-------------------------------");
+        // Java 7 implementation
+        try(ResourceAfterJava9 resource2 = new ResourceAfterJava9()){
+            resource2.op1();
+        }
+
+        // Java 9 implementation
+        use(new ResourceAfterJava9()); // we can pass any Resource class or subclass which implements AutoCloseable interface
+    }
+
+    public static void use(ResourceAfterJava9 resource){
+        // Disadvantage is this resource will be available throughout this function scope.
+        // Resource should be final or effectively final (Effectively final means we can't mutated this object)
+        //resource = resource; // it will give compilation error as resource should be final or effectively final.
+
+        try(resource){
+            resource.op1();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -9,8 +9,14 @@ This repository contains different features published in different versions of J
     - Interface can have static private method.
     - Interface can't have default private method. It's an <b>Illegal combination of modifiers default and private</b>.
 
-
 2. Effectively final in try-with-resource
+    - Garbage collector is meant to collect garbage resources. It cleans up the memory by collecting those objects which has null reference. But in our real life, we open so many external resources like DB Connection, File System, Socket, TCP Protocol etc. But GC doesn't collect any external resources.
+    - We have finalize() method but not sure when this method is called.
+    - Java 7, we have try-with-resource implementation. `try(Resource resource = new Resource()) {}`
+    - We can see this try-with-resource is tightly coupled.
+    - Java 9 provides implementation where we can pass any Resource class or subclass which is implemented AutoCloseable interface.
+    - Restriction for this implementation is Resource class should be final or Effectively final.
+    - Java 9, finalize() is deprecated.
 
 3. No _ in variable name
 
